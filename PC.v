106edf301e2e9e134fb050c56a3c7cc3d8654e82
ln05q <lenhatquang11a1@gmail.com>
2026-01-01 13:48:35 +0700

    update negative reset_n

--- PC.v
+++ PC.v
@@ -1,11 +1,11 @@
 module PC (
-	input clk, en, rst,
+	input clk, en, rst_n,
 	input [31:0] addr_in,
 	output reg [31:0] addr_out
 );
 
-	always @(posedge clk or negedge rst) begin
-		if(~rst)
+	always @(posedge clk or negedge rst_n) begin
+		if(~rst_n)
 			addr_out <= 32'b0;
 		else if(en)
 			addr_out <= addr_in;
