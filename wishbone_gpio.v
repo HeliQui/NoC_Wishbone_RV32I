1268bcf28bbd7e28cc48dddba744534719b11528
ntqui0912 <23521299@gm.uit.edu.vn>
2026-01-02 23:41:10 +0700

    Update wishbone_gpio.v

--- wishbone_gpio.v
+++ wishbone_gpio.v
@@ -16,14 +16,14 @@ module wishbone_gpio (
 );
 
     // --- 1. Internal Signals & Constants ---
-    // ??nh ngh?a 3 tr?ng thái
+    // ??nh ngh?a 3 tr?ng thÃ¡i
     localparam STATE_IDLE     = 2'b00;
     localparam STATE_ACK      = 2'b01;
-    localparam STATE_COOLDOWN = 2'b10; // Tr?ng thái ngh? an toàn
+    localparam STATE_COOLDOWN = 2'b10; 
 
-    reg [1:0] state, next_state; // T?ng lên 2 bit ?? ch?a 3 tr?ng thái
+    reg [1:0] state, next_state; 
     
-    // Tín hi?u ?i?u khi?n cho Core
+
     wire [1:0] core_addr;
     wire       core_we;
     wire [31:0] core_rdata;
@@ -31,7 +31,7 @@ module wishbone_gpio (
     // --- 2. Instantiate GPIO Core ---
     gpio_basic core (
         .clk      (clk_i),
-        .rst      (rst_n_i),    // ??o bit ?? ?úng chu?n Active High Reset c?a core
+        .rst      (rst_n_i),    
         .addr     (core_addr),
         .we       (core_we),
         .wdata    (wb_data_i),
@@ -43,7 +43,7 @@ module wishbone_gpio (
     // Mapping: 0x00->0, 0x04->1, 0x08->2
     assign core_addr = wb_addr_i[3:2]; 
 
-    // --- 4. FSM Logic (3 State - Safe Mode) ---
+    // --- 4. FSM Logic  ---
     always @(posedge clk_i or negedge rst_n_i) begin
         if (!rst_n_i) state <= STATE_IDLE;
         else          state <= next_state;
@@ -53,17 +53,13 @@ module wishbone_gpio (
         next_state = state;
         case (state)
             STATE_IDLE: begin
-                // N?u có yêu c?u h?p l? -> Chuy?n sang ACK
                 if (wb_cyc_i && wb_stb_i) 
                     next_state = STATE_ACK;
             end
             STATE_ACK: begin
-                // ?ã ACK xong -> Sang COOLDOWN (ngh? 1 nh?p)
-                // ?? ??m b?o Master k?p h? STB xu?ng
                 next_state = STATE_COOLDOWN;
             end
             STATE_COOLDOWN: begin
-                // H?t th?i gian ngh? -> Quay v? IDLE s?n sàng ?ón request m?i
                 next_state = STATE_IDLE;
             end
             default: next_state = STATE_IDLE;
@@ -71,14 +67,10 @@ module wishbone_gpio (
     end
 
     // --- 5. Output Logic ---
-    // Ch? b?t ACK khi ?ang ? tr?ng thái STATE_ACK
     assign wb_ack_o  = (state == STATE_ACK);
     
-    // D? li?u luôn ???c ??a ra bus
     assign wb_data_o = core_rdata;
 
-    // QUAN TR?NG: Ch? cho phép ghi vào Core khi b?t ??u phát hi?n request (? IDLE)
-    // Khi sang ACK hay COOLDOWN thì core_we ph?i b?ng 0 ngay.
     assign core_we = (wb_cyc_i && wb_stb_i && wb_we_i) && (state == STATE_IDLE);
 
 endmodule
